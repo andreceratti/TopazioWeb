af70e98df20f371ccabb9392664910ae39d37de5
André Ceratti da Rocha <andre.ceratti.rocha@gmail.com>
2016-05-23 10:47:55 -0300

    adicionado linhas que chamam a função pra inserir dados na tabela Pedido

--- include/pedido.php
+++ include/pedido.php
@@ -17,15 +17,23 @@
 
     if (isset($nomearquivo)){
         if (!empty($nomeantigo)){
-            if (!$tamanho>3048){
+            if ($tamanho<3145728){
                 if($tipo[1]=='jpg'||$tipo[1]=='jpeg'||$tipo[1]=='png'){
                     move_uploaded_file($nomeTemporario, $pasta.$nomearquivo.'.'.$tipo[1]);
                     echo 'Arquivo enviado com sucesso<br>';
+                    
+                    $SQL = "INSERT INTO PEDIDO"
+                            . " (CLIENTE_USUARIO_id_usuario, CLIENTE_id_cliente,"
+                            . " dt_abertura_pedido,"
+                            . " ic_delivery_sim_nao, im_foto_receita,"
+                            . " ds_status_pedidos, ic_forma_pagamento_dinheiro_cartao)"
+                            . " VALUES (?,?,NOW(),'0',?,'Esperando Orçamento',?);";
+                    insertPedido($SQL, $nomearquivo);
                 } else {
                     echo 'Aviso: Tipo de arquivo incompativel<br>';
                 }
             } else {
-                echo 'Aviso: Arquivo muito grande.';
+                echo 'Aviso: Arquivo muito grande.<br> '.$tamanho;
             }
         } else {
             echo '<h3>Por favor selecione uma imagem para enviar</h3>';
