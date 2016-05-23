5b753067b8f053c287dc532f1f50f2f38706cf85
André Ceratti da Rocha <andre.ceratti.rocha@gmail.com>
2016-05-23 08:10:58 -0300

    criado tela para envio de pedidos

---
+++ pedido/cadastrar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <?php
+            include "../include/conexao.php";
+            include '../include/pedido.php';
+        ?>
+        <title>Cadastro de Pedido</title>
+    </head>
+    <body>
+        <form method="POST" enctype="multipart/form-data">
+            <label>Pedido: <br/></label><input type="file" name="arquivo"/> <br/>
+            <p><label>Forma de Pagamento: </label><select>
+                <option>PagueSeguro</option>
+                <option>DinheiroMail</option>
+            </select></p>
+            <p><input type="submit" value="Enviar"/> </p>
+        </form>
+    </body>
+</html>
