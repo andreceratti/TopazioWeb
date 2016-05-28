913563d900977a8476e225d682cc7402657df413
André Ceratti da Rocha <andre.ceratti.rocha@gmail.com>
2016-05-28 17:15:39 -0300

    adicionado arquivo para cadastrar novas avaliações

---
+++ avaliacao/cadastrar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Avaliar Farmacia</title>
+    </head>
+    <body>
+        <form>
+            <p>
+                <label>Nota:</label>
+                <select>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </select>
+            </p>
+            <p>
+                Observação:
+                <textarea rows="4" cols="50"></textarea>
+            </p>
+        </form>
+    </body>
+</html>
