1c2ccf30cce7077923e7a7c77b69ba8f55a5b40a
Susana <Susana@DESKTOP-65GDHD3>
2016-05-29 00:03:02 -0300

    Modificação css - Tabela apilcada ao alterar cliente.

--- cliente/alterar.php
+++ cliente/alterar.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+    <meta charset="utf-8">
         <?php
             include "../include/cliente.php";
             include "../include/conexao.php";
@@ -11,9 +12,8 @@
                 alterarUsuario();
             }
         ?>
-        
-        <link href="css/estilo.css" rel="stylesheet" type="text/css"/>
-        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <link href="../css/estilo.css" rel="stylesheet" type="text/css"/>
+        <link href="../css/style.css" rel="stylesheet" type="text/css"/>
         
     </head>
     <body>
@@ -28,21 +28,37 @@
             $info = pesquisaAlterar($SQL);
             ?>
             <form method="get">
-                <input type="text" name="email" value='<?php echo $info->email ?>'<br/>
-                <select name="ativo">
-                    <option value="1" <?php if ($info->conta==1) echo 'selected="selected"' ?>>Ativada</option>
-                    <option value="0" <?php if ($info->conta==0) echo 'selected="selected"' ?>>Desativada</option>
-                </select><br/>
-                <select name="tipo">
-                    <option value="2" <?php if ($info->tipo==2) echo 'selected="selected"' ?>>Cliente</option>
-                    <option value="3" <?php if ($info->tipo==3) echo 'selected="selected"' ?>>Farmacia</option>
-                    <option value="1" <?php if ($info->tipo==1) echo 'selected="selected"' ?>>Administrador</option>
-                </select><br/>
-                Nova Senha:<input type="password" name="senha"/>
-                <input type="hidden" value="<?php echo $info->id ?>" name="id"/>
+                
+            <center><div class="center"> 
+                    
+                <table>
+                    <tr>
+                        <td><p><label>Email:</label></td>
+                        <td><input type="text" name="email" value='<?php echo $info->email ?>'</p></td>
+                    </tr>
+                    <tr>
+                    <select name="ativo">
+                        <option value="1" <?php if ($info->conta==1) echo 'selected="selected"' ?>>Ativada</option>
+                        <option value="0" <?php if ($info->conta==0) echo 'selected="selected"' ?>>Desativada</option>
+                    </select>
+                    </tr>
+                    <tr>
+                    <select name="tipo">
+                        <option value="2" <?php if ($info->tipo==2) echo 'selected="selected"' ?>>Cliente</option>
+                        <option value="3" <?php if ($info->tipo==3) echo 'selected="selected"' ?>>Farmacia</option>
+                        <option value="1" <?php if ($info->tipo==1) echo 'selected="selected"' ?>>Administrador</option>
+                    </select>
+                    </tr>
+                    <tr>
+                        <td><p><label>Nova Senha:</label></td>
+                        <td><input type="password" name="senha"/>
+                            <input type="hidden" value="<?php echo $info->id ?>" name="id"/></p></td>
+                    </tr>
+                </table>
                 <input type="submit" value="Alterar"/>
             </form>
-        
+        </center></div> 
+                
         <a href="../usuario/pesquisar.php">VOLTAR</a>
         <?php endif; ?>
     </body>
