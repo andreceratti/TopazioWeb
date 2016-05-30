c12008b9d0a3a6c7dc840811ca6e2ae16b578c67
André Ceratti da Rocha <andre.ceratti.rocha@gmail.com>
2016-05-30 07:49:08 -0300

    criado pagina de pesquisar avaliações

---
+++ avaliacao/pesquisar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+            include '../include/conexao.php';
+            include '../include/avaliacao.php';
+            include '../include/farmacia.php';
+        ?>
+        <title>Ranking das farmacias</title>
+    </head>
+    <body>
+        <form>
+            <p>
+                <label>Nome da Farmacia </label><input type="text" name="nomeFarmacia"/>
+                <input type="submit" value="Pesquisar"/>
+            </p>
+        </form>
+        
+        <table>
+            <tr>
+                <th>Nome</th>
+                <th>Pontuação</th>
+                <th>Ranking</th>
+            </tr>
+            <?php popularAvaliacao() ?>
+        </table>
+    </body>
+</html>
