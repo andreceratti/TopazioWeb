22047458ae5768b77018a16c8e44c13542459a73
Marcia <Marcia@Marcia-Note>
2016-06-05 18:36:36 -0300

    estrutura.css aplicada a cadastrar avaliação.

--- avaliacao/cadastrar.php
+++ avaliacao/cadastrar.php
@@ -9,44 +9,70 @@
         <title>Avaliar Farmacia</title>
         
         <link href="../css/estilo.css" rel="stylesheet" type="text/css"/>
-        <link href="../css/style.css" rel="stylesheet" type="text/css"/>
+        <link href="../css/style2.css" rel="stylesheet" type="text/css"/>
+        <link href="../css/layout.css" rel="stylesheet" type="text/css"/>
+        <link href="../css/estrutura.css" rel="stylesheet" type="text/css"/>
         
     </head>
     <body>
+            <div class="linha">
+                <div class="coluna col12">
+                    <div class="cabeca"> <!-- inicio cabeçalho -->
+                        <h1>Topazio Farma</h1>
+                    </div> <!-- fim cabeçalho -->
+                </div>
+            </div>
         
-        <center><div class="center">
-        <form>
-            <table>
-                <tr>
-            
-                    <td><p><label>Farmacia:</label></p></td>
-                    <td><select>
-                        <?php popularSelectFarmacia() ?>
-                        </select></td>            
-                </tr>
-                
-                <tr>            
-                    <td><p><label>Nota:</label></p></td>
-                    <td><select>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                        </select></td>
-                </tr>
-                
-                <tr>            
-                    <td><p><label>Observação:</label></p></td>
-                    <td><textarea rows="4" cols="50"></textarea></td>
-                </tr>
-            </table>
-            
-            <input type="submit" value="Enviar"/>
-        </form>
-        
-        </div></center>
+              <div class="linha">
+                <div class="coluna col12">
+                <img src="../img/topazio.png" height="300px" width="300px">
+                    <div class="corpo"> <!-- inicio corpo-->
+                        
+                        
+                <center><div class="center">
+                <form>
+                    <table>
+                        <tr>
+
+                            <td><p><label>Farmacia:</label></p></td>
+                            <td><select>
+                                <?php popularSelectFarmacia() ?>
+                                </select></td>            
+                        </tr>
+
+                        <tr>            
+                            <td><p><label>Nota:</label></p></td>
+                            <td><select>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                                </select></td>
+                        </tr>
+
+                        <tr>            
+                            <td><p><label>Observação:</label></p></td>
+                            <td><textarea rows="4" cols="50"></textarea></td>
+                        </tr>
+                    </table>
+
+                    <input type="submit" value="Enviar"/>
+                </form>
+
+                    </div></center>
+                </div> <!-- fim corpo -->                
+                </div> <!--FIM col5-->                
+            </div><!--Fim linha-->
     
+            <div class="linha">
+                <div class="coluna col12">   
+                    <div class="rodape"> <!-- inicio de rodape -->
+                        Topazio Farma - Solução de orçamentos para sua farmácia
+                    </div> <!-- fim de rodape -->                                    
+                </div> <!--FIM col12-->                
+            </div><!--Fim linha-->
+            
         <?php
             $db->close();
         ?>
