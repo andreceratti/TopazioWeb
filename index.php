321368d7ed0b7723972b2febce6e5209c8d53221
André Ceratti da Rocha <andre.ceratti.rocha@gmail.com>
2016-06-06 13:55:20 -0300

    Tentando arrumar css

--- index.php
+++ index.php
@@ -23,38 +23,58 @@
         
     </head>
     <body>
-                <?php if(empty($_SESSION["loginAtivo"])): ?>
-                    <form method="post">
-                        
+        <?php if(empty($_SESSION["loginAtivo"])): ?>
+        <form method="post">
+            <div class="linha">
+                <div class="coluna col12">
                     <div class="cabeca"> <!-- inicio cabeçalho -->
                         <h1>Topazio Farma</h1>
                     </div> <!-- fim cabeçalho -->
-                     
+                </div>
+            </div>
+                    
+            
+            <div class="linha">
+                <div class="coluna col12">
+                    <img src="img/topazio.png" height="300px" width="300px">
+                                
                     <div class="corpo"> <!-- inicio corpo-->
-                       
+                            
                         <center><div class="center"> <!-- inicio da centralização -->
 
                         <form method="post">
-
+                            
+                            
                             <p> Login: <input type="text" name="login"/> </p>
                             <p> Senha: <input type="password" name="senha"/> </p>
                             <input type="submit" value="Logar"/>
                             <p><a href="cliente/cadastrar.php">Novo Cadastro</a></p>
                         </form>
+
                     <?php else: ?>
                     <form method="post">     
                     <form method="post">
                         <b><?=$_SESSION["usuario.login"];?></b>
 
                         <input type="hidden" name="logout" value="sair"/>
-                        <input type="submit" value="Deslogar"/>
-
+                        <input type="submit" value="Deslogar"/>             
                         </div></center><!-- fim da centralização -->
                     </div> <!-- fim corpo -->
-                    
+                
+                </div>
+                
+            </div>
+            
+            
+            <div class="linha">
+                <div class="coluna col12">
                     <div class="menu"> <!-- inicio de menu -->
                     <?php if ($_SESSION["usuario.tipo"]=='1'):?>
-                    
+                        
+                                <div class="cabeca"> <!-- inicio cabeçalho -->
+                                    <h1>Topazio Farma</h1>
+                                </div> <!-- fim cabeçalho -->
+                                                  
                         <span><a href="cliente/cadastrar.php"><p>Cadastrar Cliente</p></a></span>
                         <span><a href="farmacia/cadastrar.php"><p>Cadastrar Farmacia</p></a></span>
                         <span><a href="usuario/pesquisar.php"><p>Pesquisar Usuario</p></a></span>
@@ -76,11 +96,17 @@
                     <?php endif; ?>
                     </div> <!-- fim de menu -->                    
                 </form>
-                
-                <div class="rodape"> <!-- inicio de rodape -->
-                    Topazio Farma - Solução de orçamentos para sua farmácia
-                </div> <!-- fim de rodape -->                                    
-                <?php endif; ?>
+                </div>
+            </div>
+            
+            <div class="linha">
+                <div class="coluna col12">   
+                    <div class="rodape"> <!-- inicio de rodape -->
+                        Topazio Farma - Solução de orçamentos para sua farmácia
+                    </div> <!-- fim de rodape -->                                    
+                    <?php endif; ?>
+                </div>
+            </div>
                   
     </body>
 </html>
